b2ca60acf56830d98ae08b66dc0cb6022bab2808
xuanjiao <xuanjiao.zhu@gmail.com>
2021-03-17 16:45:30 +0100

    first test semihosting

--- STM32_FreeRTOS/src/main.c
+++ STM32_FreeRTOS/src/main.c
@@ -8,12 +8,57 @@
   ******************************************************************************
 */
 
-
+#include<stdio.h>
+#include<stdint.h>
+#include<string.h>
 #include "stm32f4xx.h"
 #include "FreeRTOS.h"
 
+#include "task.h"
+
+TaskHandle_t xTaskHandle1 = NULL;
+TaskHandle_t xTaskHandle2 = NULL;
+
+void vTask1_handler(void *params);
+void vTask2_handler(void *params);
+
+extern void initialise_monitor_handles();
+
 int main(void)
 {
+	initialise_monitor_handles();
+	printf("Main function start\n");
+	/*
+	 * Resets the RCC clock configuration to the default reset state.
+	 * HSE OFF, PLL OFF, HSI ON and used as system clock source (16 MHz)
+	 * AHB, APB1 and APB2 prescaler set to 1.
+	 */
+	RCC_DeInit();
 
-	for(;;);
+	// Update SystemCoreClock variable
+	SystemCoreClockUpdate();
+
+	xTaskCreate(vTask1_handler,"Task-1",configMINIMAL_STACK_SIZE,NULL,configMAX_PRIORITIES,&xTaskHandle1);
+	xTaskCreate(vTask2_handler,"Task-2",configMINIMAL_STACK_SIZE,NULL,configMAX_PRIORITIES,&xTaskHandle1);
+
+	vTaskStartScheduler();
+
+}
+
+void vTask1_handler(void *params)
+{
+	while(1)
+	{
+		printf("task-1\n");
+		taskYIELD();
+	}
+}
+
+void vTask2_handler(void *params)
+{
+	while(1)
+	{
+		printf("task-2\n");
+		taskYIELD();
+	}
 }
